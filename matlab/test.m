f5ecae0dca00b0a6e8bd12f57026ce7c089fda0b
Mark <peiqi1122@gmail.com>
2018-05-06 17:30:05 -0400

    add

---
+++ matlab/test.m
@@ -0,0 +1,12 @@
+function [C, m, n] = addmat(A, B)
+% function definition signature
+% function [return_values, ...]  = func_name(args, ...)
+
+[m, n] = size(A);
+for i = 1:m
+    for j = 1:n
+        C(i, j) = A(i, j) + B(i, j);
+    end
+end
+
+
